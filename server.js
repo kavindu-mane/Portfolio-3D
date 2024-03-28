95d9d8531cb6164262db7954e1a2b8a17cfac267
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-03-28 17:57:02 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = process.env.NODE_ENV !== "production" ? "localhost" : "kavindu.me";
 const port = process.env.PORT || 3000;
 
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
